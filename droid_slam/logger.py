9f3e8367cfa3f3624a442e219dd96864bcb23082
alejandrofontan <fontanvillacampa@gmail.com>
2026-07-09 12:39:48 +1000

    update to loguru

--- droid_slam/logger.py
+++ droid_slam/logger.py
@@ -1,6 +1,7 @@
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
+from loguru import logger
 
 
 SUM_FREQ = 100
@@ -16,15 +17,14 @@ class Logger:
     def _print_training_status(self):
         if self.writer is None:
             self.writer = SummaryWriter('runs/%s' % self.name)
-            print([k for k in self.running_loss])
+            logger.info(f"Tracking metrics: {[k for k in self.running_loss]}")
 
         lr = self.scheduler.get_lr().pop()
         metrics_data = [self.running_loss[k]/SUM_FREQ for k in self.running_loss.keys()]
         training_str = "[{:6d}, {:10.7f}] ".format(self.total_steps+1, lr)
         metrics_str = ("{:10.4f}, "*len(metrics_data)).format(*metrics_data)
-        
-        # print the training status
-        print(training_str + metrics_str)
+
+        logger.info(training_str + metrics_str)
 
         for key in self.running_loss:
             val = self.running_loss[key] / SUM_FREQ
